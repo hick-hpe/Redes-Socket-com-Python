7967e5ad3ceff08b09852d8ca0ff6a0f283d36f0
Hick <124798752+hick-hpe@users.noreply.github.com>
2026-03-20 13:17:54 -0300

    Add files via upload

--- stop/servidor.py
+++ stop/servidor.py
@@ -7,7 +7,7 @@ HOST = "0.0.0.0"
 PORT = 9002
 
 WAITING_TIME = 3
-jogadores = []
+jogadores = {}
 alfabeto = [chr(i) for i in range(65, 91)]
 letra = None
 
@@ -17,32 +17,37 @@ def getLetra():
 
 def conectar_cliente(conn):
     global letra
+    jogadores[conn] = {}
+    
     if letra is None:
         letra = getLetra() 
-    conn.sendall(f"Letra: {letra}".encode())
+
+    mensagem = "Letra: {letra}"
+    conn.sendall(mensagem.encode())
 
 
 def receber_palavra(conn):
     resposta = conn.recv(1024).decode()
     obj = json.loads(resposta)
-    jogadores.append(obj)
+    jogadores[conn] = {
+        'pontos': 0,
+        'resposta': obj
+    }
     
 
-def calcular_pontuacao():
-    pontos = [0, 0]
-    j1 = jogadores[0]
-    j2 = jogadores[1]
-    
-    chaves = ["cep", "nome", "fruta", "animal"]
-    for chave in chaves:
-        if j1[chave] == j2[chave]:
-            pontos[0] += 1
-            pontos[1] += 1
+def calcular_pontuacao(conn1, conn2):
+    j1 = jogadores[conn1]
+    j2 = jogadores[conn2]
+
+    for chave in j1['resposta']:
+        if j1['resposta'][chave] == j2['resposta'][chave]:
+            j1['pontos'] += 1
+            j2['pontos'] += 1
         else:
-            pontos[0] += 3
-            pontos[1] += 3
+            j1['pontos'] += 3
+            j2['pontos'] += 3
 
-    return pontos
+    return [j1['pontos'], j2['pontos']]
 
 
 def iniciar_servidor():
@@ -57,30 +62,27 @@ def iniciar_servidor():
         conn2, addr2 = server.accept()
         
         # conexao
-        t1 = threading.Thread(target=conectar_cliente, args=(conn1))
-        t2 = threading.Thread(target=conectar_cliente, args=(conn2))
+        t1 = threading.Thread(target=conectar_cliente, args=(conn1,))
+        t2 = threading.Thread(target=conectar_cliente, args=(conn2,))
         
         t1.start()
         t2.start()
         
         t1.join()
         t2.join()
-        
+
         # palavras
-        t1 = threading.Thread(target=receber_palavra, args=(conn1))
-        t2 = threading.Thread(target=receber_palavra, args=(conn2))
+        t1 = threading.Thread(target=receber_palavra, args=(conn1,))
+        t2 = threading.Thread(target=receber_palavra, args=(conn2,))
         
         t1.start()
         t2.start()
         
         t1.join()
         t2.join()
-        
+
         # calcular resultado
-        pontos = calcular_pontuacao()
-        print("Jogador1:", pontos[0])
-        print("Jogador2:", pontos[1])
-        
+        pontos = calcular_pontuacao(conn1, conn2)
         conn1.sendall(f"Vc fez {pontos[0]} pontos".encode())
         conn2.sendall(f"Vc fez {pontos[1]} pontos".encode())
 
